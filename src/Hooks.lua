2ec2f11917dcc075469c4ac5ff65d5066989801c
random-facades <random.facades@gmail.com>
2025-01-21 19:25:04 -0500

    Split up Min/Max Simulations & More thorough cleaning

--- src/Hooks.lua
+++ src/Hooks.lua
@@ -5,7 +5,6 @@
 function DV.SIM.hook_functions()
    -- LHS: Balatro's default functions
    -- RHS: this mod's modified versions
-   pseudoseed = DV.SIM.new_pseudoseed
    pseudorandom = DV.SIM.new_pseudorandom
    ease_dollars = DV.SIM.new_ease_dollars
    check_for_unlock = DV.SIM.new_check_for_unlock
@@ -17,7 +16,6 @@ end
 function DV.SIM.unhook_functions()
    -- LHS: Balatro's (presumably hooked) functions
    -- RHS: this mod's saved default Balatro functions
-   pseudoseed = DV.SIM._pseudoseed
    pseudorandom = DV.SIM._pseudorandom
    ease_dollars = DV.SIM._ease_dollars
    check_for_unlock = DV.SIM._check_for_unlock
